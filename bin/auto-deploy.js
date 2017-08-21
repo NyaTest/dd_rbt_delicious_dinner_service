05087799f7133925600f1795561b17f81c65bc89
iNahooNya <346207103@qq.com>
2017-08-21 16:12:42 +0800

    publish

---
+++ bin/auto-deploy.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Koa = require("koa");
+const KoaRouter = require("koa-route");
+const child_process_1 = require("child_process");
+const path_1 = require("path");
+const root = path_1.resolve(__dirname, '../');
+const PORT = 53647;
+//https://github.com/NyaTest/dd_rbt_delicious_dinner_service/settings/hooks/
+const app = new Koa();
+app.use(KoaRouter.get('onpublish/', async (ctx) => {
+    console.log(JSON.stringify(ctx.query, null, 4));
+    child_process_1.execSync(`cd ${root} && npm run server`);
+}));
+app.listen(PORT);
+//sihgiuhwi3h29vjs 
+//# sourceMappingURL=auto-deploy.js.map
---
+++ bin/auto-deploy.ts
@@ -0,0 +1,20 @@
+import Koa = require('koa');
+import KoaRouter = require('koa-route');
+import {execSync} from 'child_process';
+import {resolve} from 'path';
+
+const root = resolve(__dirname, '../');
+
+const PORT = 53647;
+//https://github.com/NyaTest/dd_rbt_delicious_dinner_service/settings/hooks/
+
+const app = new Koa();
+
+app.use(KoaRouter.get('onpublish/', async (ctx) => {
+    console.log(JSON.stringify(ctx.query, null, 4));
+    execSync(`cd ${root} && npm run server`);
+}));
+
+app.listen(PORT);
+
+//sihgiuhwi3h29vjs
