9992194bfe273194d1007d0b13bca607fcc0ec70
iNahooNya <346207103@qq.com>
2018-06-24 09:38:41 +0800

    shut

--- mission/MissionList.js
+++ mission/MissionList.js
@@ -9,11 +9,11 @@ const Mission = require('./Mission');
 
 module.exports = [
   
-  new Mission({
-    id: 'PAIBAN_0.0.1',
-    description: '排班表',
-    interval: 24 * 60 * 60 * 1e3,
-    first: new Date('2018-06-23 09:00:00'),
-    target: 'PAIBAN/index.js'
-  })
+  // new Mission({
+  //   id: 'PAIBAN_0.0.1',
+  //   description: '排班表',
+  //   interval: 24 * 60 * 60 * 1e3,
+  //   first: new Date('2018-06-23 09:00:00'),
+  //   target: 'PAIBAN/index.js'
+  // })
 ];
